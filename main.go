303ce22723ea6d6d4f798005e4dfbe911419f4bf
ross <rostikvunarchuk@gmail.com>
2025-05-17 16:51:09 +0300

    add precommit hooks

--- main.go
+++ main.go
@@ -6,6 +6,14 @@ import (
 
 func main() {
 	fmt.Println("hello world")
+
+	sum := 0
+
+	for i := range 100_000_000 {
+		sum += i
+	}
+
+	fmt.Println(sum)
 }
 
 func strConcat(first string, second string) string {
